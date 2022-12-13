2bcdf54acfd61b3ddb03c43ec58ead5d32978e3a
Tuğkan Ayhan <tugkanayhan@gmail.com>
2022-12-13 19:20:22 +0300

    Initial Commit

---
+++ LiveCoin/LiveCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCoinApp.swift
+//  LiveCoin
+//
+//  Created by Tuğkan Ayhan on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LiveCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
